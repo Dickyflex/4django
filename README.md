965ccbc4b7a5b4a0e77946754bd6fc03c4ac46f8
AJ <ankit@primecarecorp.com>
2023-12-06 10:12:36 -0500

    second update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 4django
+---
+project-level > settings.py
+ALLOWED_HOST = ['*']
+
+---
+requirements.txt:
+django==4.1
+whitenoise
+psycopg2-binary
+
+## Commands:
+python -c "import django; print(django.get_version())"
+4.1
+
+python --version
+Python 3.11.5
+
+---
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'whitenoise.runserver_nostatic', #whitenoise
     "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware', # whitenoise
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -116,9 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # whitenoise
+STATIC_URL = "static/" 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # whitenoise
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
